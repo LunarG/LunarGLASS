84e10bf8befb067c219a91006f4aca9a2fc21250
michael-lunarg <michael@lunarg.com>
2011-02-24 18:20:59 +0000

    Added in GPL license

--- LLVM/llvm-2.8/lib/Analysis/StructuralAnalysis/StructuralAnalysis.cpp
+++ LLVM/llvm-2.8/lib/Analysis/StructuralAnalysis/StructuralAnalysis.cpp
@@ -1,12 +1,33 @@
-//===- StructuralAnalysis.cpp - region detection for structural analysis --------------------===//
+//===- StructuralAnalysis.cpp - region detection for structural analysis --===//
 //
 //                     The LLVM Compiler Infrastructure
 //
-
+// LunarGLASS: An Open Modular Shader Compiler Architecture
+// Copyright (C) 2010-2011 LunarG, Inc.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; version 2 of the
+// License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301, USA.
 //
 //===----------------------------------------------------------------------===//
-// Detects single entry single exit regions in the control flow graph,
-// including sequences.
+//
+// Author: Michael Ilseman, LunarG
+//
+//===----------------------------------------------------------------------===//
+//
+// Perform Structural Analysis, as outlined in Muchnick
+//
 //===----------------------------------------------------------------------===//
 
 #include "StructuralAnalysis.h"
--- LLVM/llvm-2.8/lib/Analysis/StructuralAnalysis/StructuralAnalysis.h
+++ LLVM/llvm-2.8/lib/Analysis/StructuralAnalysis/StructuralAnalysis.h
@@ -1,8 +1,29 @@
-//===- StructuralAnalysis.h - Structural analysis ----------------------*- C++ -*-===//
+//===- StructuralAnalysis.h - Structural analysis ------------------------===//
 //
 //                     The LLVM Compiler Infrastructure
 //
-
+// LunarGLASS: An Open Modular Shader Compiler Architecture
+// Copyright (C) 2010-2011 LunarG, Inc.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; version 2 of the
+// License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301, USA.
+//
+//===----------------------------------------------------------------------===//
+//
+// Author: Michael Ilseman, LunarG
+//
 //===----------------------------------------------------------------------===//
 //
 // Perform Structural Analysis, as outlined in Muchnick
