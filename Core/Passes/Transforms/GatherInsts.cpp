96f7d6bece54cad4330126e70e5a5a3f7f96e293
michael-lunarg <michael@lunarg.com>
2012-05-21 22:00:40 +0000

    Find and replace extended multiply patterns with mulExtended intrinsics in GatherInsts
    Example:
      %lhs = [s|z]ext i32 %operand0, i64
      %rhs = [s|z]ext i32 %operand1, i64
      %mul = mul i64 %lhs, i64 %rhs
      %res_lo = trunc i64 %mul, i32
      %shift = [l|a]shr i64 res, 32
      %res_hi = trunc i64 %shift, i32
    is replaced by:
      %mulExt = call [u|s]mulExtended %operand0, %operand1
      %res_hi = extractvalue %mulExt, 0
      %res_lo = extractvalue %mulExt, 1

--- Core/Passes/Transforms/GatherInsts.cpp
+++ Core/Passes/Transforms/GatherInsts.cpp
@@ -41,6 +41,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Pass.h"
+#include "llvm/Transforms/Utils/Local.h"
 
 #include "Passes/PassSupport.h"
 #include "Passes/Immutable/BackEndPointer.h"
@@ -75,20 +76,50 @@ namespace  {
         // Optimize min(max(a,b),c) into saturate or clamp
         bool visitMinMaxPair(IntrinsicInst* fMin, IntrinsicInst* fMax);
 
-
         bool visit(Instruction* inst);
         bool visitIntrinsic(IntrinsicInst* intr);
 
-        // When combining/optimizing intrinsics, use the deadList to keep around
-        // instructions to remove from the function. This allows iterators to be
-        // preserved when iterating over instructions.
-        std::vector<Instruction*> deadList;
+        bool visitMulExtended(BinaryOperator* imul, Value* operand0, Value* operand1, Value* highBits, Value* lowBits,
+                              bool isSigned);
+
+        // Pattern matching methods
+
+        // Try to match an extended multiply patter. Takes the multiply as
+        // input. Returns whether the multiply matches a multiple-extended
+        // pattern, along with whether it is signed and the llvm values for the
+        // original (i32) operands and high and low bits in the passed
+        // parameters operand0/operand1/high/lowBits/isSigned, when present.
+        bool matchMulExtended(/* inputs */  const Instruction* mulInst,
+                              /* outputs */ Value*& operand0, Value*& operand1, Value*& highBits, Value*& lowBits,
+                                            bool& isSigned);
+
+
+        // Whether the passed value is a trunc from i64->i32
+        bool is64To32BitTrunc(const llvm::TruncInst* trunc)
+        {
+            return trunc && gla::GetBasicType(trunc->getSrcTy()) == i64Ty
+                && gla::GetBasicType(trunc->getDestTy()) == i32Ty;
+        }
+        bool is64To32BitTrunc(const llvm::Value* truncInst)
+        {
+            const TruncInst* trunc = dyn_cast<const TruncInst>(truncInst);
+            return is64To32BitTrunc(trunc);
+        }
+
+        // When combining/optimizing intrinsics, use the deadList to keep
+        // values/instructions that we want to try to recursively delete from
+        // the function. This allows iterators to be preserved when iterating
+        // over instructions.
+        std::vector<Value*> deadList;
 
         Module* module;
         LLVMContext* context;
 
         BackEnd* backEnd;
         IRBuilder<>* builder;
+
+        const Type* i32Ty;
+        const Type* i64Ty;
 
         GatherInsts(const GatherInsts&); // do not implement
         void operator=(const GatherInsts&); // do not implement
@@ -97,9 +128,8 @@ namespace  {
 
 void GatherInsts::dce()
 {
-    for (std::vector<Instruction*>::iterator i = deadList.begin(), e = deadList.end(); i != e; ++i) {
-        (*i)->dropAllReferences();
-        (*i)->eraseFromParent();
+    for (std::vector<Value*>::iterator i = deadList.begin(), e = deadList.end(); i != e; ++i) {
+        RecursivelyDeleteTriviallyDeadInstructions(*i);
     }
 }
 
@@ -116,6 +146,9 @@ bool GatherInsts::runOnFunction(Function& F)
     module  = F.getParent();
     context = &F.getContext();
     builder = new IRBuilder<>(*context);
+
+    i32Ty = IntegerType::get(*context, 32);
+    i64Ty = IntegerType::get(*context, 64);
 
     bool changed = false;
 
@@ -135,6 +168,111 @@ bool GatherInsts::runOnFunction(Function& F)
 
     return changed;
 }
+
+bool GatherInsts::matchMulExtended(const Instruction* mul, Value*& operand0, Value*& operand1,
+                                   Value*& highBits, Value*& lowBits, bool& isSigned)
+{
+    // Match:
+    //   %lhs = [s|z]ext i32 %operand0, i64
+    //   %rhs = [s|z]ext i32 %operand1, i64
+    //   %mul = mul i64 %lhs, i64 %rhs
+    //   %res_lo = trunc i64 %mul, i32
+    //   %shift = [l|a]shr i64 res, 32
+    //   %res_hi = trunc i64 %shift, i32
+    // modulo vector-ness
+
+    lowBits = 0;
+    highBits = 0;
+    operand0 = 0;
+    operand1 = 0;
+
+    // Has to be a 64-bit multiply
+    if (mul->getOpcode() != Instruction::Mul || gla::GetBasicType(mul) != i64Ty)
+        return false;
+
+    // Operands have to be extended i32 -> i64 and they have to match in their
+    // extension (signed vs zero).
+    for (unsigned int i = 0; i < mul->getNumOperands(); ++i) {
+        Value* v = mul->getOperand(i);
+
+        if (const CastInst* ext = dyn_cast<CastInst>(v)) {
+            if ((ext->getOpcode() != Instruction::SExt && ext->getOpcode() != Instruction::ZExt)
+               || gla::GetBasicType(ext->getSrcTy()) != i32Ty || gla::GetBasicType(ext->getDestTy()) != i64Ty)
+                return false;
+
+            (i == 0 ? operand0 : operand1) = ext->getOperand(0);
+
+            isSigned = ext->getOpcode() == Instruction::SExt;
+
+        } else if (const Constant* cArg = dyn_cast<const Constant>(v)) {
+            // TODO: handle the case where a constant made its way into the mulExtended.
+            gla::UnsupportedFunctionality("extended multiply of a constant");
+        } else {
+            return false;
+        }
+    }
+
+    // Operands have to be extended similarly with respect to sign
+    const llvm::Instruction* op0Inst = llvm::dyn_cast<const llvm::Instruction>(mul->getOperand(0));
+    const llvm::Instruction* op1Inst = llvm::dyn_cast<const llvm::Instruction>(mul->getOperand(1));
+    if (op0Inst && op1Inst && op0Inst->getOpcode() != op1Inst->getOpcode())
+        return false;
+
+    // The multiply's result has to be immediately broken apart into its high
+    // and/or low bits
+    for (Value::const_use_iterator i = mul->use_begin(), e = mul->use_end(); i != e; ++i) {
+        const Instruction* use = dyn_cast<const Instruction>(*i);
+        if (! use)
+            return false;
+
+        // Try low bits
+        if (is64To32BitTrunc(*i)) {
+            // Has to be the (only) access of the lower 32 bits
+            if (lowBits)
+                return false;
+
+            lowBits = const_cast<User*>(*i);
+
+            continue;
+        }
+
+        // Try high bits
+
+        // Has to be the only access of the high bits
+        if (highBits)
+            return false;
+
+        const BinaryOperator* shift = dyn_cast<const BinaryOperator>(*i);
+        if (! shift || (shift->getOpcode() != Instruction::AShr && shift->getOpcode() != Instruction::LShr))
+            return false;
+
+        // Has to be have a constant shift amount, equal across all components
+        // if a vector.
+        const Constant* shiftBy = dyn_cast<const Constant>(shift->getOperand(1));
+        if (! shiftBy)
+            return false;
+
+        if (const ConstantVector* shiftByVec = dyn_cast<ConstantVector>(shiftBy)) {
+            shiftBy = shiftByVec->getSplatValue();
+            if (! shiftBy)
+                return false;
+        }
+
+        const ConstantInt* shiftByInt = dyn_cast<const ConstantInt>(shiftBy);
+        if (! shiftByInt || ! shiftByInt->equalsInt(32))
+            return false;
+
+        // The shift has to only be used by a trunc i64 -> i32
+        if (! shift->hasOneUse() || ! is64To32BitTrunc(shift->use_back()))
+            return false;
+
+        highBits = const_cast<Instruction*>(shift->use_back());
+    }
+
+    // We've identified a mul extended pattern
+    return true;
+}
+
 
 
 bool GatherInsts::visitMinMaxPair(IntrinsicInst* fMin, IntrinsicInst* fMax)
@@ -211,6 +349,40 @@ bool GatherInsts::visitMinMaxPair(IntrinsicInst* fMin, IntrinsicInst* fMax)
     return true;
 }
 
+bool GatherInsts::visitMulExtended(BinaryOperator* imul, Value* operand0, Value* operand1,
+                                   Value* highBits, Value* lowBits, bool isSigned)
+{
+    assert(imul->getOpcode() == Instruction::Mul && gla::GetBasicType(imul->getType()) == i64Ty
+           && operand0 && operand1 && operand0->getType() == operand1->getType()
+           && gla::GetBasicType(operand0->getType()) == i32Ty);
+
+    // Make the mulExtended
+    builder->SetInsertPoint(imul);
+    const Type* type = operand0->getType();
+    const Type* tys[4] = { type, type, type, type };
+    Intrinsic::ID id = isSigned ? Intrinsic::gla_smulExtended : Intrinsic::gla_umulExtended;
+    Function* f = Intrinsic::getDeclaration(module, id, tys, 4);
+    Instruction* mulExtended = builder->CreateCall2(f, operand0, operand1);
+
+    // Make the extracts, update the code to use the new values, and delete the
+    // old code.
+    // Note that the deletion of values in the deadList is recursive, and thus
+    // will also delete the mul and the [s|z]ext instructions.
+    if (highBits) {
+        Value* extractHigh = builder->CreateExtractValue(mulExtended, 0);
+        highBits->replaceAllUsesWith(extractHigh);
+        deadList.push_back(highBits);
+    }
+
+    if (lowBits) {
+        Value* extractLow = builder->CreateExtractValue(mulExtended, 1);
+        lowBits->replaceAllUsesWith(extractLow);
+        deadList.push_back(lowBits);
+    }
+
+    return true;
+}
+
 
 bool GatherInsts::visitIntrinsic(IntrinsicInst* intr)
 {
@@ -240,23 +412,28 @@ bool GatherInsts::visitIntrinsic(IntrinsicInst* intr)
 
 bool GatherInsts::visit(Instruction* inst)
 {
-    bool changed = false;
-
     if (IntrinsicInst* intr = dyn_cast<IntrinsicInst>(inst)) {
         return visitIntrinsic(intr);
     }
 
     switch (inst->getOpcode()) {
 
-    case Instruction::SExt:
-    case Instruction::ZExt:
-        // TODO
-        ;
+    case Instruction::Mul: {
+        Value* operand0;
+        Value* operand1;
+        Value* highBits;
+        Value* lowBits;
+        bool isSigned;
 
+        if (matchMulExtended(inst, operand0, operand1, highBits, lowBits, isSigned))
+            return visitMulExtended(cast<BinaryOperator>(inst), operand0, operand1, highBits, lowBits, isSigned);
+
+        break;
+    }
 
     } // end of switch (inst->getOpcode())
 
-    return changed;
+    return false;
 }
 
 void GatherInsts::getAnalysisUsage(AnalysisUsage& AU) const
