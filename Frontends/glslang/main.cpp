c2a2588902a46f265a42336fda8dc2c66049fb59
johnkslang <cepheus@frii.com>
2013-02-06 23:04:14 +0000

    glslang front end: make use of new front-end ability to set the default version (eg., ES vs. desktop) when no #version is provided.

--- Frontends/glslang/main.cpp
+++ Frontends/glslang/main.cpp
@@ -269,7 +269,7 @@ bool CompileFile(char *fileName, ShHandle compiler, int debugOptions, const TBui
     for (int i = 0; i < 1000; ++i) {
         for (int j = 0; j < 100; ++j)
 #endif
-            ret = ShCompile(compiler, data, OutputMultipleStrings, EShOptNone, resources, debugOptions);
+            ret = ShCompile(compiler, data, OutputMultipleStrings, EShOptNone, resources, debugOptions, 110);
 #ifdef MEASURE_MEMORY
 
         GetProcessMemoryInfo(GetCurrentProcess(), &counters, sizeof(counters));
