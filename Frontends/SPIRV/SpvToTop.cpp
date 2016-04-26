d3cce8f97f877d98b004eeb627d1d987ab3eb4c6
GregF <greg@LunarG.com>
2016-04-25 18:05:38 -0600

    Add metadata qualifiers for VertexIndex and InstanceIndex

--- Frontends/SPIRV/SpvToTop.cpp
+++ Frontends/SPIRV/SpvToTop.cpp
@@ -913,7 +913,9 @@ gla::EMdInputOutput SpvToTopTranslator::getMdQualifier(spv::Id resultId) const
     case gla::EmbPointSize:   mdQualifier = gla::EMioPointSize;      break;
     case gla::EmbClipVertex:  mdQualifier = gla::EMioClipVertex;     break;
     case gla::EmbInstanceId:  mdQualifier = gla::EMioInstanceId;     break;
+    case gla::EmbInstanceIndex: mdQualifier = gla::EMioInstanceIndex; break;
     case gla::EmbVertexId:    mdQualifier = gla::EMioVertexId;       break;
+    case gla::EmbVertexIndex: mdQualifier = gla::EMioVertexIndex;    break;
     case gla::EmbFragCoord:   mdQualifier = gla::EMioFragmentCoord;  break;
     case gla::EmbPointCoord:  mdQualifier = gla::EMioPointCoord;     break;
     case gla::EmbFace:        mdQualifier = gla::EMioFragmentFace;   break;
