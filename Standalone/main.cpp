828a1f4e021e910257cce248ba94ef2cf0adb46d
GregF <greg@LunarG.com>
2016-05-18 15:10:06 -0600

    test: enable Bottom IR and GLSL generation and comparison

--- Standalone/main.cpp
+++ Standalone/main.cpp
@@ -654,21 +654,21 @@ void TranslateLinkedShaders(const std::vector<const char*>& names)
                     manager.dump("\nTop IR:\n");
 
                 // Generate the Bottom IR
-                //manager.translateTopToBottom();
+                manager.translateTopToBottom();
     
-                //if (Options & EOptionAssembly)
-                    //manager.dump("\n\nBottom IR:\n");
+                if (Options & EOptionAssembly)
+                    manager.dump("\n\nBottom IR:\n");
 
                 // Generate the GLSL output
-                //manager.translateBottomToTarget();
+                manager.translateBottomToTarget();
 
                 // Get and print the generated GLSL output
-                //if (! (Options & EOptionMemoryLeakMode) && ! (Options & EOptionSilent)) {
-                //    if (Options & EOptionDumpIndexShader)
-                //        printf("%s\n", manager.getIndexShader());
-                //    else if (manager.getGeneratedShader())
-                //        printf("%s\n", manager.getGeneratedShader());
-                //}
+                if (! (Options & EOptionMemoryLeakMode) && ! (Options & EOptionSilent)) {
+                    if (Options & EOptionDumpIndexShader)
+                        printf("%s\n", manager.getIndexShader());
+                    else if (manager.getGeneratedShader())
+                        printf("%s\n", manager.getGeneratedShader());
+                }
             }
             #ifdef _WIN32
                 if (Options & EOptionMemoryLeakMode) {
